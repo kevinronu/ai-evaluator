2c4023d011bce0a5e5c6a089065a58a216b61238
Kevin Robles <kevinronu@gmail.com>
2025-06-07 20:26:52 -0500

    Chore: Add env schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      OPEN_AI_APIK_KEY: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
+});
